3b0dc754698083e1947a8d6fddbef7f835fd8505
smitchhatrola2505 <schhatrola779@rku.ac.in>
2022-12-23 18:15:59 +0530

    23/12/2022

--- Spice/Data/ApplicationDbContext.cs
+++ Spice/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Spice.Models;
 
 namespace Spice.Data
 {
@@ -9,5 +10,6 @@ namespace Spice.Data
             : base(options)
         {
         }
+        public DbSet<Category> Category { get; set; }
     }
 }
--- Spice/Data/Migrations/20221223122605_addCategoryToDatabase.Designer.cs
+++ Spice/Data/Migrations/20221223122605_addCategoryToDatabase.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Spice.Data;
 
@@ -11,9 +12,11 @@ using Spice.Data;
 namespace Spice.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221223122605_addCategoryToDatabase")]
+    partial class addCategoryToDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
